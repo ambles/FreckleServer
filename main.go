89908c10b4f2b00fd041ead6cf657ff8451c7f28
Ambles Kwok <ambles.kwok@gmail.com>
2015-11-12 21:11:00 -0500

    Add -p=80 support.

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
 
 	router := NewRouter()
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	var port int
+	flag.IntVar(&port, "p", 8080, "specify port to use.  defaults to 8080.")
+	flag.Parse()
+
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), router))
 }

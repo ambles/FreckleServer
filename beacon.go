494be90f6dc559a4886bef3d8d6e3867d58d15da
Ambles Kwok <ambles.kwok@gmail.com>
2015-11-17 15:00:00 -0500

    Adding list DB call -- VERY first version!

---
+++ beacon.go
@@ -0,0 +1,11 @@
+package main
+
+import "time"
+
+type Beacon struct {
+	ID string `json:"id"`
+	//Completed bool      `json:"completed"`
+	CreationDate time.Time `json:"due"`
+}
+
+type Beacons []Beacon
--- handlers.go
+++ handlers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 	"os"
 	"time"
@@ -17,7 +19,77 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	case "ping", "Ping":
 		fmt.Fprintln(w, "We've gone a ", action, " event")
 	case "list", "List":
-		fmt.Fprintln(w, "We've gone a", action, "event!")
+
+		// TODO: user and dbname should be in configuration
+		db, err := sql.Open("postgres", "user=postgres dbname=freckle_proximity_db sslmode=disable")
+		if err != nil {
+			log.Fatal(err)
+		}
+		rows, err := db.Query("SELECT * FROM beacons")
+		if err != nil {
+			panic(err.Error())
+		}
+
+		var (
+			beaconid         string
+			date             time.Time
+			uuid             string
+			major            int
+			minor            int
+			nickname         []byte
+			current_campaign []byte
+			inredis          bool
+			active           bool
+			lat              float64
+			long             float64
+			location         []byte
+			tags             []byte
+			attributes       []byte
+			class            []byte
+			creation_date    time.Time
+			geom             []byte
+		)
+
+		beacons := Beacons{}
+
+		defer rows.Close()
+		for rows.Next() {
+			err := rows.Scan(
+				&beaconid,         // 1
+				&date,             // 2
+				&uuid,             // 3
+				&major,            // 4
+				&minor,            // 5
+				&nickname,         // 6
+				&current_campaign, // 7
+				&inredis,          // 8
+				&active,           // 9
+				&lat,              // 10
+				&long,             // 11
+				&location,         // 12
+				&tags,             // 13
+				&attributes,       // 14
+				&class,            // 15
+				&creation_date,    // 16
+				&geom,             // 17
+			)
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Println(beaconid)
+			beacons = append(beacons, Beacon{ID: beaconid, CreationDate: creation_date})
+		}
+		err = rows.Err()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(beacons); err != nil {
+			panic(err)
+		}
+
 	case "enter", "Enter":
 		fmt.Fprintln(w, "We've gone a ", action, " event")
 	case "exit", "Exit":
